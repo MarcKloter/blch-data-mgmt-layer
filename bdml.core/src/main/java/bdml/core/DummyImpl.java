b9e9bc3b65d3acd8052a1c895496ef503b80521a
Marc <marc.kloter@bluewin.ch>
2018-12-23 22:51:23 +0100

    implemented createAccount

--- bdml.core/src/main/java/bdml/core/DummyImpl.java
+++ bdml.core/src/main/java/bdml/core/DummyImpl.java
@@ -2,11 +2,14 @@ package bdml.core;
 
 import bdml.cache.CacheImpl;
 import bdml.services.Cache;
+import bdml.services.api.Core;
 
 public class DummyCache {
 
 	public static void main(String[] args) {
-		Cache cache = new CacheImpl();
+		//Cache cache = new CacheImpl();
+		Core core = new CoreImpl();
+		core.createAccount("myPassword");
 	}
 
 }
