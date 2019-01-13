2e9c83eca107077491550668867179162bf39778
Marc <marc.kloter@bluewin.ch>
2019-01-13 20:57:26 +0100

    replaced AES/CBC/PKCS5Padding with AES/GCM/NoPadding

--- bdml.core/src/main/java/bdml/core/helper/Crypto.java
+++ bdml.core/src/main/java/bdml/core/helper/Crypto.java
@@ -1,57 +1,69 @@
 package bdml.core.helper;
 
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.util.Arrays;
 
 import bdml.services.exceptions.MisconfigurationException;
 import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class Crypto {
-    // TODO: load application.properties config
+    // TODO: load default.application.properties config
     private static final String HASH_FUNCTION = "SHA-256";
     private static final String SYMMETRIC_ALGORITHM = "AES";
-    // using PKCS#7 padding (identifier in the SUN provider misleading)
-    private static final String SYMMETRIC_CIPHER = "AES/CBC/PKCS5Padding";
+    private static final String SYMMETRIC_CIPHER = "AES/GCM/NoPadding";
+    private static final int AUTHENTICATION_TAG_SIZE = 128;
+    private static final int IV_BYTES = 12;
 
+    /**
+     *
+     * @param capability
+     * @param plaintext
+     * @return
+     */
     public static byte[] symmetricallyEncrypt(byte[] capability, byte[] plaintext) {
         SecretKeySpec key = new SecretKeySpec(capability, SYMMETRIC_ALGORITHM);
 
         // generate initialization vector
-        byte[] ivBytes = new byte[16];
+        byte[] ivBytes = new byte[IV_BYTES];
         new SecureRandom().nextBytes(ivBytes);
-        IvParameterSpec iv = new IvParameterSpec(ivBytes);
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(AUTHENTICATION_TAG_SIZE, ivBytes);
 
         try {
             Cipher cipher = Cipher.getInstance(SYMMETRIC_CIPHER);
-            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+            cipher.init(Cipher.ENCRYPT_MODE, key, parameterSpec);
             byte[] ciphertext = cipher.doFinal(plaintext);
 
             // append iv to ciphertext
             return concatenate(ciphertext, ivBytes);
         } catch (GeneralSecurityException e) {
-            // TODO: handle exception for wrong key seperately (return null)
             throw new MisconfigurationException(e.getMessage());
         }
     }
 
+    /**
+     *
+     * @param capability
+     * @param ciphertext
+     * @return
+     */
     public static byte[] symmetricallyDecrypt(byte[] capability, byte[] ciphertext) {
         SecretKeySpec key = new SecretKeySpec(capability, SYMMETRIC_ALGORITHM);
 
         // read initialization vector from ciphertext
-        int index = ciphertext.length - 16;
+        int index = ciphertext.length - IV_BYTES;
         byte[] ivBytes = Arrays.copyOfRange(ciphertext, index, ciphertext.length);
         ciphertext = Arrays.copyOf(ciphertext, index);
-        IvParameterSpec iv = new IvParameterSpec(ivBytes);
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(AUTHENTICATION_TAG_SIZE, ivBytes);
 
         try {
             Cipher cipher = Cipher.getInstance(SYMMETRIC_CIPHER);
-            cipher.init(Cipher.DECRYPT_MODE, key, iv);
+            cipher.init(Cipher.DECRYPT_MODE, key, parameterSpec);
             return cipher.doFinal(ciphertext);
+        } catch(InvalidKeyException e) {
+            throw new IllegalArgumentException(e.getMessage());
         } catch (GeneralSecurityException e) {
-            // TODO: handle exception for wrong key seperately (return null)
             throw new MisconfigurationException(e.getMessage());
         }
     }
@@ -73,6 +85,10 @@ public class Crypto {
 
     /**
      * Concatenate two byte arrays.
+     *
+     * @param a first byte array to concatenate
+     * @param b second byte array to concatenate
+     * @return byte array of length {@code a.length + b.length} containing the content of {@code a} and {@code b}.
      */
     public static byte[] concatenate(byte[] a, byte[] b) {
         byte[] c = new byte[a.length + b.length];
