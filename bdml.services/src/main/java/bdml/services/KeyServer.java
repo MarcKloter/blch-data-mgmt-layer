5b45eba0a11d539ae0a86c5ad454dc79393d2462
Marc <marc.kloter@bluewin.ch>
2019-01-19 12:42:13 +0100

    added unit tests for bdml.keyserver

--- bdml.services/src/main/java/bdml/services/KeyServer.java
+++ bdml.services/src/main/java/bdml/services/KeyServer.java
@@ -3,15 +3,21 @@ package bdml.services;
 import java.security.PublicKey;
 
 public interface KeyServer {
-    // TODO: javadoc
-
+    /**
+     * Registers the given {@link PublicKey} at the connected key server.
+     *
+     * @param identifier String to identify the key for retrieval
+     * @param key public key to register
+     * @throws IllegalArgumentException if the given {@code identifier} has been registered before.
+     * @throws NullPointerException if {@code identifier} or {@code key} is {@code null}.
+     */
     void registerKey(String identifier, PublicKey key);
 
     /**
      * Returns the key associated with the given identifier or null.
      *
-     * @param identifier
-     * @return
+     * @param identifier String that identifies a public key stored in the connected key server
+     * @return {@link PublicKey} associated with the given {@code identifier} or {@code null}.
      */
     PublicKey queryKey(String identifier);
 }
